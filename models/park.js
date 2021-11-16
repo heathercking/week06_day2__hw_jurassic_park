4b41b6d572f310464e238b95c17f0b8f26879ab6
Heather King <hkingdevelopment@gmail.com>
2021-11-16 18:36:27 +0000

    addds findDinosaurWithMostVisitors method to Park

--- models/park.js
+++ models/park.js
@@ -12,4 +12,17 @@ Park.prototype.removeADinosaur = function(index) {
     this.dinosaurs.splice(index, 1);
 };
 
+Park.prototype.findDinosaurWithMostVisitors = function() {
+    let highestGuests = 0;
+    let topDinosaur;
+
+    for (const dinosaur of this.dinosaurs) {
+        if (dinosaur.guestsAttractedPerDay > highestGuests) {
+            topDinosaur = dinosaur
+            highestGuests = dinosaur.guestsAttractedPerDay
+        };
+    };
+    return topDinosaur;
+};
+
 module.exports = Park;
